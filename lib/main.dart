dda47bfbc68c3bff2f100dfe7b141051566e1911
Philip Boardman <dustykeyboard@brd.mn>
2022-07-31 10:26:16 +1000

    Add .env support for environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:my_app/utils/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:my_app/pages/splash_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  await dotenv.load();
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
     // TODO: Replace credentials with your own
-    url: 'SUPABASE_URL',
-    anonKey: 'SUPABASE_ANON_KEY',
+    url: dotenv.env['SUPABASE_URL'],
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'],
   );
   runApp(const MyApp());
 }
